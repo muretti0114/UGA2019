3b8e98302583e428cc560d8196046194466f4b13
Bastien Zigmann <bastien.zigmann@xsalto.com>
2019-05-29 15:06:06 +0900

    Removal of api_key from source code. Internet connection checking. Error handling debug.

--- Decision.py
+++ Decision.py
@@ -1,7 +1,18 @@
-import urllib2
+# import urllib2
+import urllib
+import urllib.request
+import urllib.error
 # If you are using Python 3+, import urllib instead of urllib2
 
 import json
+
+def isConnected():
+    try :
+        stri = "https://www.google.co.in"
+        data = urllib.request.urlopen(stri)
+        return True
+    except urllib.request.URLError:
+        return False
 
 # Season conversion
 def parseSeason(seasonNb):
@@ -32,6 +43,10 @@ print("--------------------------------------------------------------")
 seasonParsed = -2
 while(seasonParsed == -2):
     season = input("Please enter the actual season \n1:winter\n2:spring\n3:summer\n4:fall\n")
+    try:
+        season = int(season)
+    except ValueError:
+        print("Wrong entry")
     seasonParsed = parseSeason(season)
 
 print("--------------------------------------------------------------")
@@ -39,36 +54,56 @@ print("--------------------------------------------------------------")
 # Getting age then converting it to be in [0,1]
 age = 0
 while(age < 18 or age >36):
-    age = input("Please enter your age (must be between 18 and 36) : ")
+    entry = input("Please enter your age (must be between 18 and 36) : \n")
+    try:
+        age = int(entry)
+    except ValueError:
+        print("Wrong entry")
 age = (age-18)/18.
 
 print("--------------------------------------------------------------")
-# Getting informations about childish diseases
 
+# Getting informations about childish diseases
 childishDiseases = -1
 while(childishDiseases != 0 and childishDiseases != 1):
-    childishDiseases = input("Did you had childish diseases (for instance chicken pox, measles, mumps, polio ..)?\n0:yes\n1:no\n")
+    entry = input("Did you had childish diseases (for instance chicken pox, measles, mumps, polio ..)?\n0:yes\n1:no\n")
+    try:
+        childishDiseases = int(entry)
+    except ValueError:
+        print("Wrong entry")
 
 print("--------------------------------------------------------------")
 
 # Getting informations about accident or serious trauma
 trauma = -1
 while(trauma != 0 and trauma != 1):
-    trauma = input("Did you had any accident or serious trauma ?\n0:yes\n1:no\n")
+    entry = input("Did you had any accident or serious trauma ?\n0:yes\n1:no\n")
+    try:
+        trauma = int(entry)
+    except ValueError:
+        print("Wrong entry")
 
 print("--------------------------------------------------------------")
 
 # Getting informations about surgical intervention
 surgical = -1
 while(surgical != 0 and surgical != 1):
-    surgical = input("Did you had any surgical intervention ?\n0:yes\n1:no\n")
+    entry = input("Did you had any surgical intervention ?\n0:yes\n1:no\n")
+    try:
+        surgical = int(entry)
+    except ValueError:
+        print("Wrong entry")
 
 print("--------------------------------------------------------------")
 
 #Getting informations about potential high fever in the past year
 fever = -2
 while(fever != -1 and fever != 0 and fever != 1):
-    fever = input("Did you had high fever during last year?\n-1:less than three months ago\n 0:more than 3 months ago\n 1:no\n")
+    entry = input("Did you had high fever during last year?\n-1:less than three months ago\n 0:more than 3 months ago\n 1:no\n")
+    try:
+        fever = int(entry)
+    except ValueError:
+        print("Wrong entry")
 
 print("--------------------------------------------------------------")
 
@@ -76,21 +111,33 @@ print("--------------------------------------------------------------")
 alcohol = -1
 while(alcohol == -1):
     alcoholInfo = input("What's your alcohol consumption frequency ?\n1:several times a day\n2:every day\n3:several times a week\n4:once a week\n5:hardly ever or never\n")
-    alcohol = parseAlcoholConsumption(alcoholInfo)
+    try:
+        alcoholInfo = int(alcoholInfo)
+        alcohol = parseAlcoholConsumption(alcoholInfo)
+    except ValueError:
+        print("Wrong entry")
 
 print("--------------------------------------------------------------")
 
 #Getting informations about smoking habit
 smoking = -2
 while(smoking != -1 and smoking != 0 and smoking != 1):
-    smoking = input("Are you smoking?\n-1:never\n 0:occasionnally\n 1:daily\n")
+    entry = input("Are you smoking?\n-1:never\n 0:occasionnally\n 1:daily\n")
+    try:
+        smoking = int(entry)
+    except ValueError:
+        print("Wrong entry")
 
 print("--------------------------------------------------------------")
 
 #Getting informations about time spent sit in a day
 hoursSit = -1
 while(hoursSit < 0 or hoursSit > 24):
-    hoursSit = input("How much do you spend sit per day?\n")
+    entry = input("How much do you spend sit per day?\n")
+    try:
+        hoursSit = int(entry)
+    except ValueError:
+        print("Wrong entry")
 hoursSit = hoursSit/24.
 
 print("--------------------------------------------------------------")
@@ -111,36 +158,49 @@ data =  {
 body = str.encode(json.dumps(data))
 
 url = 'https://ussouthcentral.services.azureml.net/workspaces/958400274ab54dac8d87114c07515abb/services/fa83783ce736417f94d3864ecf63b461/execute?api-version=2.0&details=true'
-api_key = 'noMUJzdsos/oKEH5XyMGVRESg9Z6V/lirKtaz6oE5O1t79NirCLVi4jUuuoK3FJxRVvY43Ii+cMMlulb0DkpPw=='
+with open('api_key.txt','r') as file:
+    api_key = file.read().replace('\n','')
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-req = urllib2.Request(url, body, headers)
+if(isConnected()):
 
-try:
-    response = urllib2.urlopen(req)
-    # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
-    # req = urllib.request.Request(url, body, headers)
-    # response = urllib.request.urlopen(req)
+    # req = urllib2.Request(url, body, headers)
+    req = urllib.request.Request(url, body, headers)
 
-    result = response.read()
-    # Uncomment the line bellow to see the full json of the answer
-    # print(result)
-    resultParsed = json.loads(result)
-    if(resultParsed["Results"]["output1"]["value"]["Values"][0][0] == 'N'):
-        print("It's the good time to have a baby !")
-    else:
-        print("Your fertility seems altered.")
-        print("The best season for fertility is winter")
-        if(childishDiseases == 0 or surgical == 0 or trauma == 0):
-            print("You should go for a check")
-        if(fever == -1 or fever == 0):
-            print("You should wait for few month")
-        if(smoking >= 0 or alcohol <= 3):
-            print("Maybe you should try to change your habbits (less drinking or smoking for example)")
-except urllib2.HTTPError, error:
-    print("The request failed with status code: " + str(error.code))
+    try:
+        # response = urllib2.urlopen(req)
+        response = urllib.request.urlopen(req)
+        # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+        # req = urllib.request.Request(url, body, headers)
+        # response = urllib.request.urlopen(req)
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
+        result = response.read().decode('utf-8')
+        # Uncomment the line bellow to see the full json of the answer
+        # print(result)
+        resultParsed = json.loads(result)
+        if(resultParsed["Results"]["output1"]["value"]["Values"][0][0] == 'N'):
+            print("It's the good time to have a baby !")
+        else:
+            print("Your fertility seems altered.")
+            print("The best season for fertility is winter")
+            if(childishDiseases == 0 or surgical == 0 or trauma == 0):
+                print("You should go for a check")
+            if(fever == -1 or fever == 0):
+                print("You should wait for few month")
+            if(smoking >= 0 or alcohol <= 3):
+                print("Maybe you should try to change your habbits (less drinking or smoking for example)")
 
-    print(json.loads(error.read()))
+        response2 = urllib.request.urlopen(req)
+        result2 = response2.read().decode('utf-8')
+        if(result != result2):
+            print("Your result seems to be altered, don't take it as true")
+
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+
+        print(json.loads(error.read().decode('utf-8')))
+else:
+    print("No internet connection, try again with internet connected")
